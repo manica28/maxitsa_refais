44a44f92814d0218be70c35da7c3b59003b57241
manica28 <mbayenianga28@gmail.com>
2025-07-29 11:03:52 +0000

    Update Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -12,14 +12,19 @@ class Database{
     private  static Database|null $instance = null;
 
       protected function __construct() {
+
+          //         DSN=pgsql:host=dpg-d1v830ali9vc73besm10-a.oregon-postgres.render.com;port=5432;dbname=maxittsa
+// DB_USER=postgres_z0d6_user
+// DB_PASSWORD=HAjA4dvUdXRqSKNEp2uJCSnpWm6BeFsO
+          $dsn='pgsql:host=dpg-d1v830ali9vc73besm10-a.oregon-postgres.render.com;port=5432;dbname=maxittsa';
+          $user='postgres_z0d6_user';
+          $password='HAjA4dvUdXRqSKNEp2uJCSnpWm6BeFsO';
         
 
         try {
            
-            $this->connection = new PDO(
-              DSN,
-              DB_USER,
-              DB_PASSWORD,
+            $this->connection = new PDO($dsn, $user, $password
+             
               [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
